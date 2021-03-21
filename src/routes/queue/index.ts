efc34dab701f5dc544bdfd756f4e774555445f71
Srdjan Hajder <igorisrdjan@gmail.com>
2021-03-21 22:18:50 +0100

    Start implementing types through and through

--- src/routes/queue/index.ts
+++ src/routes/queue/index.ts
@@ -1,17 +1,55 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
+import * as fastify from 'fastify'
+
+import  {
+    FastifyReply,
+    FastifyRequest,
+    FastifyInstance,
+} from "fastify"
 import { AmqpService } from "../../services/amqp-service"
+import { NameParam } from "./schema"
 
 export default function (as: AmqpService) {
     return async function (server: FastifyInstance) {
 
-        server.get('/:queue/check', async (req: FastifyRequest, res: FastifyReply) => {
-            console.log("Did this")
-            return as.checkQueue((req.params as any).queue)
-        })
+        server.get<any, NameParam, unknown>(
+            '/:queue',{
+                schema:{
+                    
+                }
+            },
+            async (req: FastifyRequest, res: FastifyReply) => {
+                console.log("Checked the queue for existence")
+                return as.checkQueue((req.params as any).queue)
+            })
 
         server.post('/:queue', async (req: FastifyRequest, res: FastifyReply) => {
             console.log(`Creating a new queue ${(req.params as any).queue}`)
             return as.assertQueue((req.params as any).queue)
+        })
+
+        server.post('/:queue/bind', async (req: FastifyRequest, res: FastifyReply) => {
+            console.log(`Binding a queue ${(req.params as any).queue}`)
+            // TODO: Assert exchange first
+            return as.bindQueue(
+                (req.params as any).query,
+                (req.body as any).exchange,
+                (req.body as any).routingKey
+            )
+        })
+
+        server.post('/:queue/unbind', async (req: FastifyRequest, res: FastifyReply) => {
+            console.log(`unbinding a queue ${(req.params as any).queue}`)
+            // TODO: Assert exchange first
+            return as.bindQueue(
+                (req.params as any).query,
+                (req.body as any).exchange,
+                (req.body as any).routingKey
+            )
+        })
+
+        server.delete('/:queue', async (req: FastifyRequest, res: FastifyReply) => {
+            console.log("Purged the queue")
+            return as.purgeQueue((req.params as any).queue)
         })
     }
 }
---
+++ src/routes/queue/schema.ts
@@ -0,0 +1,8 @@
+export type NameParam = {
+  query:string
+}
+
+export type BindBody = {
+  exchange: string,
+  routingKey: string
+}
