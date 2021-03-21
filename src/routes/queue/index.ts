557d1d3a853ae024f723d1599bf45aec56f93a60
Srdjan Hajder <igorisrdjan@gmail.com>
2021-03-21 17:12:23 +0100

    Update queue routes

--- src/routes/queue/index.ts
+++ src/routes/queue/index.ts
@@ -4,13 +4,13 @@ import { AmqpService } from "../../services/amqp-service"
 export default function (as: AmqpService) {
     return async function (server: FastifyInstance) {
 
-        server.get(':queue/check', async (req: FastifyRequest, res: FastifyReply) => {
+        server.get('/:queue/check', async (req: FastifyRequest, res: FastifyReply) => {
             console.log("Did this")
             return as.checkQueue((req.params as any).queue)
         })
 
-        server.post(':queue', async (req: FastifyRequest, res: FastifyReply) => {
-            console.log("Creating a new queue")
+        server.post('/:queue', async (req: FastifyRequest, res: FastifyReply) => {
+            console.log(`Creating a new queue ${(req.params as any).queue}`)
             return as.assertQueue((req.params as any).queue)
         })
     }
